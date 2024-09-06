0d43a4e839047c746aa3298345edc6cd9651daec
Raquel Guerreiro <96666396+RaquelGuerreiro21@users.noreply.github.com>
2024-09-06 16:13:23 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,38 @@
+let dados = [
+    {
+        titulo: "Neurociência",
+        descricao: "A neurociência é a área do conhecimento que se dedica ao estudo do sistema nervoso, com foco especial no cérebro. Através de diversas técnicas e ferramentas, os neurocientistas buscam compreender como o cérebro funciona, como processa informações, controla o comportamento e gera a mente.",
+        link: "https://pt.wikipedia.org/wiki/Neuroci%C3%AAncia",
+        tags: "neurociência, cérebro, sistema nervoso, pesquisa cerebral, cognição"
+    },
+    {
+        titulo: "Inteligência Artificial na Biologia",
+        descricao: "A inteligência artificial está revolucionando a biologia, permitindo a análise de grandes volumes de dados genômicos e a descoberta de novos tratamentos para doenças. Ferramentas de aprendizado de máquina são usadas para modelar sistemas biológicos complexos e acelerar a pesquisa.",
+        link: "https://www.google.com/search?q=inteligência+artificial+na+biologia",
+        tags: "inteligência artificial, biologia computacional, bioinformática, aprendizado de máquina, deep learning, genômica"
+    },
+    {
+        titulo: "Tecnologia na Pesquisa Médica",
+        descricao: "A tecnologia desempenha um papel crucial na pesquisa médica, desde o desenvolvimento de novos equipamentos de diagnóstico até a criação de terapias personalizadas. A nanotecnologia, a bioimpressão 3D e a realidade virtual são algumas das áreas que estão transformando a medicina.",
+        link: "https://www.google.com/search/html",
+        tags: "tecnologia médica, diagnóstico, terapia personalizada, nanotecnologia, bioimpressão 3D, realidade virtual"
+    },
+    {
+        titulo: "Engenharia Genética",
+        descricao: "A engenharia genética é a ciência que permite a manipulação direta do material genético de um organismo, permitindo a introdução, remoção ou alteração de genes específicos Essa tecnologia revolucionária tem transformado diversos setores, da medicina à agricultura. Através de técnicas como a CRISPR-Cas9, os cientistas podem editar o DNA com precisão, ",
+        link: "https://www.embrapa.br/busca-de-noticias/-/asset_publisher/BR_EBR_NOTICIAS/content/engenharia-genetica-e-biotecnologia-um-olhar-sobre-o-futuro",
+        tags: "engenharia genética, edição genética, CRISPR-Cas9, biotecnologia, DNA, genética"
+      },
+      {
+        titulo: "Biotecnologia na Agricultura",
+        descricao: "Utilização de técnicas biológicas para melhorar a produção agrícola, aumentar a resistência a doenças e desenvolver novos produtos.",
+        link: "https://www.embrapa.br/busca-de-noticias/-/asset_publisher/BR_EBR_NOTICIAS/content/biotecnologia-na-agricultura-uma-revolucao-silenciosa",
+        tags: "biotecnologia, agricultura, produção agrícola, resistência a doenças, biotecnologia vegetal"
+      },
+      {
+        titulo: "Tipos de Inteligência Artificial",
+        descricao: "Uma breve introdução aos principais tipos de algoritmos de aprendizado de máquina, como redes neurais convolucionais (CNNs), redes neurais recorrentes (RNNs) e LSTM.",
+        link: "https://niara.ai/blog/tipos-de-inteligencia-artificial/",
+        tags: "inteligência artificial, aprendizado de máquina, redes neurais, CNN, RNN, LSTM"
+      }
+];
